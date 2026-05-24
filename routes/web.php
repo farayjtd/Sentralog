2446b17e54d714d13290ae90ad6389156464b5cb
Farayjtd <farayjtd@gmail.com>
2026-05-24 19:04:26 +0700

    add routes configuration

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Halaman Utama
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () { return view('admin.index'); });
+    Route::get('/karyawan', function () { return view('admin.karyawan'); });
+    Route::get('/warehouse', function () { return view('admin.warehouse'); });
+    Route::get('/tasks', function () { return view('admin.tasks'); });
+    Route::get('/inventory', function () { return view('admin.inventory'); });
 });
